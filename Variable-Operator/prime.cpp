c48eb5a951aba7eed449b4fedd76d2ba27c38316
tusherbhakta <tusheranta@gmail.com>
2025-09-10 11:52:22 +0530

    Prime number and sum Practice QS

--- Variable-Operator/prime.cpp
+++ Variable-Operator/prime.cpp
@@ -6,7 +6,7 @@ int main(){
     bool isPrime=true;
     cout<<"Enter a number: ";
     cin>>n;
-    for(int i=2; i<n; i++){
+    for(int i=2; i*i<=n; i++){
         if(n%i==0){
             isPrime=false;
             
